57eb7177931c83a87bd87ebdab1c421484412195
danielj-rp <danielj@reactpoland.com>
2020-03-22 22:54:43 +0100

    feat: add routing

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const path = require("path");
 const app = new express();
 
 const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "login.html"));
+});
+
+app.get("/logout", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "logout.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
