99489dfd615bb92a017d5975f3413296c7c576b6
danielj-rp <danielj@reactpoland.com>
2020-03-23 09:20:08 +0100

    tests(login page): no password provided

--- cypress/integration/loginapp.spec.js
+++ cypress/integration/loginapp.spec.js
@@ -13,6 +13,15 @@ describe("UI TESTS", () => {
     cy.get("[data-cy=login-text]").should("have.length", 1);
     cy.get("[data-cy=homepage]").should("have.length", 0);
   });
+  it("should not allow login when password is not provided", () => {
+    cy.visit("http://localhost:3000");
+    cy.get("[data-cy=password-input]").should("have.length", 1);
+    cy.get("[data-cy=email-input]").type("jarosz.dan@gmail.com");
+    cy.get("[data-cy=login-button]").click();
+    //check that we are still on the same page
+    cy.get("[data-cy=login-text]").should("have.length", 1);
+    cy.get("[data-cy=homepage]").should("have.length", 0);
+  });
 });
 
 //Welcome, you are now logged in
