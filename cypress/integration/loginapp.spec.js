32f9dfaa35768f189bb395e1cbdf4fefe35e5674
danielj-rp <danielj@reactpoland.com>
2020-03-23 09:17:37 +0100

    tests(login page): no email provided

--- cypress/integration/loginapp.spec.js
+++ cypress/integration/loginapp.spec.js
@@ -1,3 +1,18 @@
 describe("UI TESTS", () => {
-  it("should navigate to Google's website", () => {});
+  it("should load the login page correctly", () => {
+    cy.visit("http://localhost:3000");
+    cy.get("[data-cy=login-text]").should("have.length", 1);
+    cy.get("[data-cy=login-text]").should("be.visible");
+  });
+  it("should not allow login when username is not provided", () => {
+    cy.visit("http://localhost:3000");
+    cy.get("[data-cy=email-input]").should("have.length", 1);
+    cy.get("[data-cy=password-input]").type("1234");
+    cy.get("[data-cy=login-button]").click();
+    //check that we are still on the same page
+    cy.get("[data-cy=login-text]").should("have.length", 1);
+    cy.get("[data-cy=homepage]").should("have.length", 0);
+  });
 });
+
+//Welcome, you are now logged in
