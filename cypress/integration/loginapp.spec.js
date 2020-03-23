10b5f20eac42a9e2e7d0fe2419707ace61a09f7f
danielj-rp <danielj@reactpoland.com>
2020-03-23 09:22:53 +0100

    tests(login page): redirect to homepage if email and password are provided

--- cypress/integration/loginapp.spec.js
+++ cypress/integration/loginapp.spec.js
@@ -4,6 +4,7 @@ describe("UI TESTS", () => {
     cy.get("[data-cy=login-text]").should("have.length", 1);
     cy.get("[data-cy=login-text]").should("be.visible");
   });
+
   it("should not allow login when username is not provided", () => {
     cy.visit("http://localhost:3000");
     cy.get("[data-cy=email-input]").should("have.length", 1);
@@ -13,6 +14,7 @@ describe("UI TESTS", () => {
     cy.get("[data-cy=login-text]").should("have.length", 1);
     cy.get("[data-cy=homepage]").should("have.length", 0);
   });
+
   it("should not allow login when password is not provided", () => {
     cy.visit("http://localhost:3000");
     cy.get("[data-cy=password-input]").should("have.length", 1);
@@ -21,6 +23,16 @@ describe("UI TESTS", () => {
     //check that we are still on the same page
     cy.get("[data-cy=login-text]").should("have.length", 1);
     cy.get("[data-cy=homepage]").should("have.length", 0);
+  });
+
+  it("should login to the homepage with valid creds", () => {
+    cy.visit("http://localhost:3000");
+    cy.get("[data-cy=email-input]").type("jarosz.dan@gmail.com");
+    cy.get("[data-cy=password-input]").type("1234");
+    cy.get("[data-cy=login-button]").click();
+    //check that we are on the homepage
+    cy.get("[data-cy=login-text]").should("have.length", 0);
+    cy.get("[data-cy=homepage]").should("have.length", 1);
   });
 });
 
